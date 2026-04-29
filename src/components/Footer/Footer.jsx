4643c4091a76ba4b59e32f3d249a1f275bc55a87
ishikapawar49-blip <ishikapawar49@gmail.com>
2026-04-29 20:24:47 +0530

    Responsive changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 import { HiOutlineBuildingOffice2 } from "react-icons/hi2";
 import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
 
@@ -20,23 +21,22 @@ const Footer = () => {
             with our cloud-based platform.
           </p>
 
-          {/* Social Icons */}
-<div className="social-icons">
-  <div className="icon"><FaFacebookF /></div>
-  <div className="icon"><FaTwitter /></div>
-  <div className="icon"><FaInstagram /></div>
-  <div className="icon"><FaYoutube /></div>
-</div>
+          <div className="social-icons">
+            <div className="icon"><FaFacebookF /></div>
+            <div className="icon"><FaTwitter /></div>
+            <div className="icon"><FaInstagram /></div>
+            <div className="icon"><FaYoutube /></div>
+          </div>
         </div>
 
         {/* Quick Links */}
         <div className="footer-col">
           <h4>Quick Links</h4>
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Features</li>
-            <li>Contact</li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/features">Features</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
           </ul>
         </div>
 
@@ -44,10 +44,10 @@ const Footer = () => {
         <div className="footer-col">
           <h4>Features</h4>
           <ul>
-            <li>Billing & Payments</li>
-            <li>Complaint Tracking</li>
-            <li>Visitor Management</li>
-            <li>Notices & Alerts</li>
+            <li><Link to="/features">Billing & Payments</Link></li>
+            <li><Link to="/features">Complaint Tracking</Link></li>
+            <li><Link to="/features">Visitor Management</Link></li>
+            <li><Link to="/features">Notices & Alerts</Link></li>
           </ul>
         </div>
 
@@ -55,9 +55,9 @@ const Footer = () => {
         <div className="footer-col">
           <h4>Contact</h4>
           <ul>
-            <li>info@societyms.com</li>
-            <li>+91 98765 43210</li>
-            <li>Nagpur, Maharashtra, India</li>
+            <li><a href="mailto:info@societyms.com">info@societyms.com</a></li>
+            <li><a href="tel:+919876543210">+91 98765 43210</a></li>
+            <li><Link to="/contact">Nagpur, Maharashtra, India</Link></li>
           </ul>
         </div>
 
